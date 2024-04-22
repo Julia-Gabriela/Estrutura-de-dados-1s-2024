168beedbe53efc443c88f6a03c74a9a068ac0bee
- <->
2024-04-21 23:16:36 -0300

    Atividade 4

--- Lista_encadeada/main.c
+++ Lista_encadeada/main.c
@@ -7,32 +7,22 @@ int main(int argc, char* argv[]){
     No* H = no('A', no('B', no('C', no('D', no('E', no('F', NULL))))));
     No* Hc = copiar_lista(H);
 
-    No* H = no('A', NULL);
-    No* n2 = no('B', NULL);
-    No* n3 = no('C', NULL);
-    No* n4 = no('D', NULL);
-    No* n5 = no('E', NULL);
-    No* n6 = no('F', NULL);
-
-    inserir_no(H, n2);
-    inserir_no(H, n3);
-    inserir_no(H, n4);
-    inserir_no(H, n5);
-    inserir_no(H, n6);
-
     imprimir_lista(H);
-    printf("\n");
+    printf("\n copia\n");
     imprimir_lista(Hc);
 
-    H->proximo_no->valor ='X';
-    Hc->valor ='U';
+    H->proximo_no->valor = 'X';
+    Hc->valor = 'U';
 
-    imprimir_lista(H);
     printf("\n");
+    imprimir_lista(H);
+    printf("\n copia\n");
     imprimir_lista(Hc);
 
-    printf("\n qnt = %d", qnt_nos(H));
-    printf("\n copia qnt = %d", qnt_nos(Hc));
+    liberar_lista(Hc);
+    
+    printf("\n qtd = %d", quantidade_nos(H));
+    printf("\n copia qtd = %d", quantidade_nos(Hc));
 
     exit(0);
 }
